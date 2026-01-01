8b0ffaf227a81dfcbafcc2d385e3efbbbb8927d8
Brian Magazu <bmagazu@gmail.com>
2026-01-01 13:18:35 -0500

    added save/ to gitignore + WASD+E plugin
    prevents our playtest saves and custom options settings etc from getting pushed with builds... all save/ files get created anew by player

---
+++ js/plugins/OG_WASDwEasOKdefault.js
@@ -0,0 +1,28 @@
+/*:
+ * @target MZ
+ * @plugindesc Doors - Default keybinds (E = OK, PageUp/PageDown on their keys)
+ * @author Overhill Games
+ * @help
+ * Put this BELOW VisuStella Core Engine + Options Core if using.
+ * Defines shipped defaults. Players can still rebind if enabled.
+ */
+
+(() => {
+  const KEY = {
+    E: 69,
+    PAGEUP: 33,
+    PAGEDOWN: 34
+  };
+
+  const _Scene_Boot_start = Scene_Boot.prototype.start;
+  Scene_Boot.prototype.start = function() {
+    _Scene_Boot_start.call(this);
+
+    // Set shipped defaults:
+    Input.keyMapper[KEY.E] = "ok";
+
+    // If you still want pageup/pagedown to exist, map them to the actual keys:
+    Input.keyMapper[KEY.PAGEUP] = "pageup";
+    Input.keyMapper[KEY.PAGEDOWN] = "pagedown";
+  };
+})();
